ae83e1ccee57d23a3421cfd802ea82578598a617
Tse, Christopher <ctse@netsuite.com>
2019-11-20 15:58:45 -0600

    Add knexfile with env

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,10 @@
-require('dotenv').config();
 const pg = require('pg');
+
+if (process.env.NODE_ENV === 'development') {
+	require('dotenv').config();
+} else {
+	pg.defaults.ssl = true;
+}
 
 // TODO: Create env variable
 module.exports = {
