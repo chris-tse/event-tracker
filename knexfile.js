3ef2c4d23b629a188a6db7835697a52d5fe241f0
Christopher Tse <chris@chris-tse.com>
2019-11-30 12:26:07 -0600

    Update knexfile to use new connections

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,12 @@
 const pg = require('pg');
 
+// Comment out this line if using local database
+pg.defaults.ssl = true;
+
 if (process.env.NODE_ENV === 'development') {
 	require('dotenv').config();
-} else {
-	pg.defaults.ssl = true;
 }
 
-// TODO: Create env variable
 module.exports = {
 	client: 'pg',
 	connection: process.env.DATABASE_URL,
