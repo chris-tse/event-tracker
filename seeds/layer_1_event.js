d8b8b4800db31a12091ac62cee677b31f48ab6cc
Christopher Tse <chris@chris-tse.com>
2020-03-05 01:02:50 -0600

    Fix insert for times

--- seeds/layer_1_event.js
+++ seeds/layer_1_event.js
@@ -11,12 +11,12 @@ exports.seed = knex => {
 				{
 					meetup_id: '267087235',
 					title: 'Promises and Async/Await with Vance Lucas',
-					start_time: 1583694000,
+					start_time: knex.raw('to_timestamp(1583694000)'),
 				},
 				{
 					meetup_id: '267439534',
 					title: 'GraphQL with Thomas Evans',
-					start_time: 1583170200,
+					start_time: knex.raw('to_timestamp(1583170200)'),
 				},
 			]);
 		});
